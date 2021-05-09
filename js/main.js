f4f6e6728be25fb5e2d125a2b020ad1137f28d0e
tama819 <83630814+tama819@users.noreply.github.com>
2021-05-09 16:44:58 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
 
+{
+const open = document.getElementById('open');
+const overlay = document.querySelector('.overlay');
+const close = document.getElementById('close');
 
-const pageTopBtn = document.getElementById('pagetop');
-pageTopBtn.addEventListener("click", function(){
-  const me = arguments.callee;
-  const nowY = window.pageYOffset;
-  window.scrollTo(0, nowY - 100);
-  if (nowY > 0) {
-    window.setTimeout(me, 10);
-  }
+open.addEventListener('click', () => {
+  overlay.classList.add('show');
+  open.classList.add('hide');
 });
+
+close.addEventListener('click', () => {
+  overlay.classList.remove('show');
+  open.classList.remove('hide');
+});
+
+}
+
+{
+  const pageTopBtn = document.getElementById('pagetop');
+  pageTopBtn.addEventListener("click", function(){
+    const me = arguments.callee;
+    const nowY = window.pageYOffset;
+    window.scrollTo(0, nowY - 100);
+    if (nowY > 0) {
+      window.setTimeout(me, 10);
+    }
+  });
+
+}
+
+
+
+
+
