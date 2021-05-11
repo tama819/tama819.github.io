9bee9fd3e8656963147ab104304c15159901240d
tama819 <83630814+tama819@users.noreply.github.com>
2021-05-12 00:02:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
 
 {
-const open = document.getElementById('open');
-const overlay = document.querySelector('.overlay');
-const close = document.getElementById('close');
-
-open.addEventListener('click', () => {
-  overlay.classList.add('show');
-  open.classList.add('hide');
-});
-
-close.addEventListener('click', () => {
-  overlay.classList.remove('show');
-  open.classList.remove('hide');
-});
-}
-
-'use strict';
-{
   window.addEventListener('load' , function () {
     viewSlide('.slide img');
   });
@@ -53,6 +36,25 @@ close.addEventListener('click', () => {
 }
 
 
+{
+  const open = document.getElementById('open');
+  const photo = document.getElementById('photo');
+  const overlay = document.querySelector('.overlay');
+  const close = document.getElementById('close');
+  
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+    photo.classList.add('hide');
+  });
+  
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+    photo.classList.remove('hide');
+  });
+  }
+  
 
 
 
