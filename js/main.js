28a018479d3c84d48ee48680bb8fa9fddcc614b2
tama819 <83630814+tama819@users.noreply.github.com>
2021-05-11 22:09:55 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,7 +13,31 @@ close.addEventListener('click', () => {
   overlay.classList.remove('show');
   open.classList.remove('hide');
 });
+}
 
+'use strict';
+{
+  window.addEventListener('load' , function () {
+    viewSlide('.slide img');
+  });
+  let slideNo = 0;
+  function viewSlide(className, slideNo = -1) 
+  {
+    let imgArray = document.querySelectorAll(className);
+    if (slideNo >= 0) {
+      // 初回以外は現在のスライドを消す
+      imgArray[slideNo].style.opacity = 0;
+    }
+
+    slideNo++;
+    if (slideNo >= imgArray.length) {
+      slideNo = 0;
+    }
+
+    imgArray[slideNo].style.opacity = 1;
+    let msec = document.getElementById('slide_speed').value;
+    setTimeout(function(){viewSlide(className, slideNo);}, msec);
+  }
 }
 
 {
